abb234141dd3786ade96856863f48ec28a388856
fralo <leonardo.fralini@gmail.com>
2022-03-05 11:20:09 +0100

    made endpoint to retrive files info

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class ApiController extends Controller
+{
+    public function index() {
+        $files = collect(Storage::files('uploaded'));
+        $result = $files->map(function ($file, $key) {
+            return $this->getFileInfo($file);
+        });
+        return response()->json($result);
+    }
+
+    private function getFileInfo($file) {
+        $fileName = basename($file);
+        $fileSize = Storage::size($file);
+        $fileLastModified = new Carbon(Storage::lastModified($file));
+        
+        return [
+            'name' => $fileName,
+            'size' => $fileSize,
+            'last_modified' => $fileLastModified->toDateTimeString(),
+        ];
+    }
+}
